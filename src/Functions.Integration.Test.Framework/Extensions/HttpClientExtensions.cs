a2f421ec636d807d8e1f3d454ae370af4461094f
Chris Berthold <chris.berthold@jackson.com>
2023-02-17 22:04:49 -0500

    add client extension for checking if the host is running

---
+++ src/Functions.Integration.Test.Framework/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions.Integration.Test.Framework.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<bool> IsHostRunningAsync(this HttpClient client)
+        {
+            using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, string.Empty))
+            {
+                try
+                {
+                    using (HttpResponseMessage response = await client.SendAsync(request))
+                    {
+                        return response.StatusCode == HttpStatusCode.NoContent || response.StatusCode == HttpStatusCode.OK;
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
