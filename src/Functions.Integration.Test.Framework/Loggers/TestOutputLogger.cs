56883e032836224bbed3e517bce84ddc90b94bff
Chris Berthold <chris.berthold@jackson.com>
2023-02-17 22:02:30 -0500

    add test output logger for using with xunit

---
+++ src/Functions.Integration.Test.Framework/Loggers/TestOutputLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace Functions.Integration.Test.Framework.Loggers
+{
+    public class TestOutputLogger : ILogger
+    {
+        public TestOutputLogger(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public ITestOutputHelper OutputHelper { get; }
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            OutputHelper.WriteLine(formatter(state, exception));
+        }
+    }
+}
---
+++ src/Functions.Integration.Test.Framework/Loggers/TestOutputLoggerProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace Functions.Integration.Test.Framework.Loggers
+{
+    public class TestOutputLoggerProvider : ILoggerProvider
+    {
+        public TestOutputLoggerProvider(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+        }
+
+        public ITestOutputHelper OutputHelper { get; }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new TestOutputLogger(OutputHelper);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
